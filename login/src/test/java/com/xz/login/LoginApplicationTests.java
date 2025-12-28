a6fdaf79109747ed281f0f4593bc8d43495fbeb1
xuezhao <13201433786@163.com>
2025-12-28 20:32:37 +0800

    Initial commit

---
+++ login/src/test/java/com/xz/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xz.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
