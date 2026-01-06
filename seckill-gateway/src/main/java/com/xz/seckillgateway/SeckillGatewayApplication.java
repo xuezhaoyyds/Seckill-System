a1ccc3227dd88ebae74a92631512b064991ebe96
xuezhao <13201433786@163.com>
2026-01-06 19:48:49 +0800

    基础网关服务

---
+++ seckill-gateway/src/main/java/com/xz/seckillgateway/SeckillGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xz.seckillgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillGatewayApplication.class, args);
+    }
+
+}
---
+++ seckill-gateway/src/test/java/com/xz/seckillgateway/SeckillGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xz.seckillgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
